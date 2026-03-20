b0d731e12ed1409415e825b7a3239559eb22c915
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2026-03-20 09:26:48 +0100

    crear endpoint Get para obterner los links

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,23 +1,41 @@
-// Importamos la librería Express
 const express = require('express');
+const initDB = require('./database/db');
 
-// Exportar la funcion de iniciar la BD, desde el archivo especificado
-const initDB = require('./database/db')
-
-// Iniciamos la aplicación
 const app = express();
-
-// Definir el puerto donde va a escuchar el servidor
 const PORT = 3000;
 
-// Crear la ruta principal (cuando entres a localhost:3000)
+// 1º ENDPOINT (GET /api/links)
+app.get('/api/links', (req, res) => {
+  // Creamos un array con un par de objetos simulando lo que leeremos de SQLite en el futuro
+  const datosDePrueba = [
+    {
+      id: 1,
+      titulo: "Documentación de React",
+      url: "https://react.dev",
+      descripcion: "La web oficial para aprender React",
+      imagen: "https://react.dev/favicon.ico",
+      categoria_id: null
+    },
+    {
+      id: 2,
+      titulo: "MDN Web Docs",
+      url: "https://developer.mozilla.org",
+      descripcion: "Referencia para HTML, CSS y JS",
+      imagen: null,
+      categoria_id: null
+    }
+  ];
+
+  res.json(datosDePrueba);
+});
+
+// LA RUTA RAÍZ
 app.get('/', (req, res) => {
   res.send('¡Hola Mundo desde el backend de SmartMark!');
 });
 
-// Encender el servidor y la BD
+// ARRANQUE DEL SERVIDOR
 app.listen(PORT, async () => {
-  console.log(`Servidor de SmartMark corriendo perfectamente en http://localhost:${PORT}`);
-
-  await initDB()
+  console.log(`Servidor de SmartMark corriendo en http://localhost:${PORT}`);
+  await initDB();
 });
